bf9be1c574f233e169f57d826a07f0684790858a
Naufal189 <naufaldzakwan189@gmail.com>
2025-09-04 14:52:03 +0700

    migrations

--- app/Models/post.php
+++ app/Models/post.php
@@ -2,45 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 use function Pest\Laravel\post;
 
-class Post
+class Post extends Model //terhubung ke tabel posts
 {
-    public static function all()
-    {
-        return [
-            [
-                'id'    => 1,
-                'slug' => 'judul-article-1',
-                'title' => 'Judul Article 1',
-                'author' => 'Naufal Dzakwan',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et rerum nulla debitis, mollitia atque libero ut! Maxime libero explicabo hic, minima exercitationem, esse numquam, temporibus sit aliquam sint odio eveniet?',
-            ],
-            [
-                'id'    => 2,
-                'slug' => 'judul-article-2',
-                'title' => 'Judul Article 2',
-                'author' => 'Naufal Dzakwan',
-                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit eveniet accusamus odio aut. Laborum voluptates harum nemo saepe dignissimos laudantium vero, maxime, ipsam, nobis molestias tempora nesciunt ipsa assumenda expedita.',
-            ]
-        ];
-    }
 
-    public static function find($slug): array
-    {
-        // callback
-        // return  Arr::first(static::all(), function ($post) use ($slug) {
-        //     return $post['slug'] == $slug;
-        // });
-
-        // arrow function
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (! $post) {
-            abort(404);
-        }
-        return $post;
-    }
 }
