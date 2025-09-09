1ead427def587b4b8bba8f4bc9b58c1efe4e5edf
Naufal189 <naufaldzakwan189@gmail.com>
2025-09-09 09:44:18 +0700

    Model Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,9 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            // $a = $a ? $a : $b; // ternary operator
+            // $a = $a ?: $b;     // elvis operator
+            // $a ??= $b          // null coalescing operator
             'remember_token' => Str::random(10),
         ];
     }
@@ -37,7 +40,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
