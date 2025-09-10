d9d1f1825d1e18134e9bdb8287857299c9dcbb16
Naufal189 <naufaldzakwan189@gmail.com>
2025-09-10 15:14:21 +0700

    N + 1

--- app/Models/post.php
+++ app/Models/post.php
@@ -11,6 +11,8 @@ class Post extends Model //terhubung ke tabel posts
     use HasFactory;
     protected $fillable = ['title', 'author', 'slug', 'body'];
 
+    protected $with = ['author', 'category'];
+
     public $timestamps = true; // default-nya true, pastikan tidak false
 
     public function author(): BelongsTo
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 Route::get('/posts', function () {
+
+    // Eager Loading Atau N + 1
+    // $post = Post::with(['author', 'category'])->latest()->get();
+    $post = Post::latest()->get();
     return view('posts', ['title' => 'Blog', 'posts' => post::all()]);
 });
 
@@ -18,10 +22,15 @@ Route::get('/posts/{post:slug}', function (post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+
+    // Lazy Eager Loading
+    // $posts = $user->posts->load('category', 'author');
     return view('posts', ['title' => count($user->posts) . ' Articles By ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+
+    // $posts = $category->posts->load('category', 'author');
     return view('posts', ['title' => ' Articles In ' . $category->name, 'posts' => $category->posts]);
 });
 
